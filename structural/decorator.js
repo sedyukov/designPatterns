ea5aab9828d4dbc3dbddb01b9c2173101c1145df
sedyukov <sedyukovyaroslav@gmail.com>
2022-07-03 18:49:44 +0700

    feature: add decorator

---
+++ structural/decorator.js
@@ -0,0 +1,29 @@
+class Server {
+  constructor(ip, port) {
+    this.ip = ip;
+    this.port = port;
+  }
+
+  get url() {
+    return `https://${this.ip}:${this.port}`
+  }
+}
+
+function aws(server) {
+  server.isAWS = true
+  server.awsInfo = function () {
+    return server.url;
+  }
+  return server
+}
+function azure(server) {
+  server.isAzure = true
+  server.port += 500
+  return server
+}
+const s1 = aws(new Server('12.34.56.78', 8080))
+const s2 = azure(new Server('12.23.23.23',8080))
+console.log(s1.isAWS)
+console.log(s1.awsInfo())
+console.log(s2.isAzure)
+console.log(s2.port)
